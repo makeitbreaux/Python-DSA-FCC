36d984d7bc0e90d2374cf9c4b20eab8bf09c46d2
Briana Breaux <72421941+makeitbreaux@users.noreply.github.com>
2022-06-12 22:24:44 +0000

    Added more methods to the LinkedList class

--- linked_list.py
+++ linked_list.py
@@ -46,6 +46,11 @@ class LinkedList:
     self.head = new_node
 
   def search(self, key):
+    '''
+    Search for the first node containing data that matches the key.
+    Returns the node or None if not found.
+    Takes O(n) time (linear).
+    '''
     current = self.head
 
     while current:
@@ -53,6 +58,33 @@ class LinkedList:
         return current
       else:
         current = current.next_node
+    return None
+
+  def insert(self, data, index):
+    '''
+    Inserts a new Node containing data at index position
+    Insertion takes O(1) time but finding node at insertion position          takes O(n) time.
+    Takes overall O(n) time.
+    '''
+    if index == 0:
+      self.add(data)
+
+    if index > 0:
+      new = Node(data)
+
+      position = index
+      current = self.head
+
+      while position > 1:
+        current = current.next_node
+        position -= 1
+
+      prev_node = current
+      next_node = current.next_node
+
+      prev_node.next_node = new
+      new.next_node = next_node
+      
   
   def __repr__(self):
     '''
@@ -72,3 +104,4 @@ class LinkedList:
 
       current = current.next_node
     return '-> '.join(nodes)
+
